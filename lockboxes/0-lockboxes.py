1aaf3cee90db75628f7c0cd4399775cbaf5af4fc
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-01-05 13:20:48 +0100

    pep8

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-'''You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1
-   and each box may contain keys to the other boxes.
+'''Have n number of locked boxes in front of you. Each box is numbered
+   sequentially from 0 to n-1 and each box may contain keys to the other boxes
    Write a method that determines if all the boxes can be opened.
    - Prototype: def canUnlockAll(boxes)
    - boxes is a list of lists
@@ -10,6 +10,7 @@
    - The first box boxes[0] is unlocked
    - Return True if all boxes can be opened, else return False
 '''
+
 
 def canUnlockAll(boxes):
     newList = []
