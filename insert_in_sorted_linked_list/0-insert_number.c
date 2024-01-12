efdf5f53d0c5d423195bd1c04e3fcbda8c5df0f1
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-01-12 10:58:12 +0100

    update

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -5,7 +5,7 @@
 /**
  * Write a function that inserts a number into a sorted singly linked list.
  * Prototype: listint_t *insert_node(listint_t **head, int number);
- * Return: the address of the new node, or NULL if it failed
+ * Return: the address of the new node, or NULL if it failed.
  */
 
 
