35c9f4b0b2a1c256eb2098898a1d39153bd02b15
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-02-02 11:06:15 +0100

    betty

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -3,9 +3,11 @@
  * Function that computes the sum of two sandpiles :
  * Prototype: void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
  * That both grid1 and grid2 are individually stable
- * A sandpile is considered stable when none of its cells contains more than 3 grains
+ * A sandpile is considered stable when none of its cells
+   contains more than 3 grains
  * When the function is done, grid1 must be stable
- * grid1 must be printed before each toppling round, only if it is unstable (See example)
+ * grid1 must be printed before each toppling round,
+   only if it is unstable (See example)
  */
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
@@ -16,9 +18,9 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
 		{
-			grid1[i][j] += grid2[i][j];                         /* add cells */
-			grid2[i][j] = (grid1[i][j] > 3);    /* map cell state onto grid2 */
-			unstable += (grid1[i][j] > 3);         /* count cell if unstable */
+			grid1[i][j] += grid2[i][j];
+			grid2[i][j] = (grid1[i][j] > 3);
+			unstable += (grid1[i][j] > 3);
 		}
 
 	/* STABILIZE SANDPILE */
@@ -26,19 +28,18 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	{
 		print_grid(grid1);
 
-		/* stabilize sandpile (using grid2 as map to find unstable cells) */
 		for (i = 0; i < 3; i++)
 			for (j = 0; j < 3; j++)
 				if (grid2[i][j])
 				{
-					grid1[i][j] -= 4;           /* remove 4 grains from cell */
-					if (i + 1 < 3)                /* add grain to right cell */
+					grid1[i][j] -= 4;
+					if (i + 1 < 3)
 						grid1[i + 1][j] += 1;
-					if (i - 1 >= 0)                /* add grain to left cell */
+					if (i - 1 >= 0)
 						grid1[i - 1][j] += 1;
-					if (j + 1 < 3)                  /* add grain to top cell */
+					if (j + 1 < 3)
 						grid1[i][j + 1] += 1;
-					if (j - 1 >= 0)              /* add grain to bottom cell */
+					if (j - 1 >= 0)
 						grid1[i][j - 1] += 1;
 				}
 
@@ -46,8 +47,8 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		for (unstable = 0, i = 0; i < 3; i++)
 			for (j = 0; j < 3; j++)
 			{
-				grid2[i][j] = (grid1[i][j] > 3);     /* map state onto grid2 */
-				unstable += (grid1[i][j] > 3);       /* count unstable cells */
+				grid2[i][j] = (grid1[i][j] > 3);
+				unstable += (grid1[i][j] > 3);
 			}
 	}
 }
