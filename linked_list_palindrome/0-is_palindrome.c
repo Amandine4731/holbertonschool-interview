1679669fa2e437409c7ef9a3d4c4f9609e6cbca5
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-02-09 15:23:24 +0100

    Linked list palindrome

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * is_palindrome - checks if a singly linked list is a palindrome.
  * @head: head of list
@@ -8,53 +9,57 @@
  * An empty list is considered a palindrome
 **/
 
-
 int is_palindrome(listint_t **head) {
-
     int length_list = 1;
     listint_t *temp;
     temp = *head;
 
     if (*head == NULL) {
-		return (1);
-	}
+        return (1);
+    }
 
-    while((*head)->next != NULL) {
+    while ((*head)->next != NULL) {
         *head = (*head)->next;
         length_list++;
     }
 
     *head = temp;
-    
+
     /* fail if the list is impair */
     if (length_list % 2 != 0) {
         return (0);
     }
 
-    int *First_part = (int*)malloc((length_list / 2) * sizeof(int));
-    int *Second_part = (int*)malloc((length_list / 2) * sizeof(int));
-    
-    for(int i = 0; i < (length_list / 2); i++) {
+    int *First_part = (int *)malloc((length_list / 2) * sizeof(int));
+    int *Second_part = (int *)malloc((length_list / 2) * sizeof(int));
+
+    int i;
+    for (i = 0; i < (length_list / 2); i++) {
         First_part[i] = (*head)->n;
         *head = (*head)->next;
     }
-    
 
-    for(int j = 0; j < (length_list / 2); j++) {
+    int j;
+    for (j = 0; j < (length_list / 2); j++) {
         Second_part[j] = (*head)->n;
         *head = (*head)->next;
     }
+
     int fi = 0;
     int si = (length_list / 2 - 1);
 
-    while(fi < (length_list / 2)) {
-        if(First_part[fi] == Second_part[si]) {
+    while (fi < (length_list / 2)) {
+        if (First_part[fi] == Second_part[si]) {
         } else {
+            free(First_part);
+            free(Second_part);
             return 0;
         }
         fi++;
         si--;
     }
-    return 1;
 
+    free(First_part);
+    free(Second_part);
+    return 1;
 }
