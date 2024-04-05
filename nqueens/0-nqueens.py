4c53e55ce5a51213632bdb942ed4f1fc5082bb0f
Amandine4731 <5618@holbertonstudents.com>
2024-04-05 01:56:22 -0700

    N queens error managment

---
+++ nqueens/0-nqueens.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" - The N queens puzzle is the challenge
+    of placing N non-attacking queens on an N×N chessboard.
+    - Write a program that solves the N queens problem.
+"""
+
+
+import sys
+
+
+def nqueens(N):
+    """ N queens """
+
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N\n')
+        exit(1)
+
+    try:
+        N = int(N)
+    except ValueError:
+        print('N must be a number\n')
+        exit(1)
+
+    if N < 4:
+        print('N must be at least 4\n')
+        exit(1)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N\n')
+        exit(1)
+    nqueens(sys.argv[1])
